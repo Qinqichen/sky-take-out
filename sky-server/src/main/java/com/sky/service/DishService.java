dff025e93245d2a57b172db7bc4a8f58cd86b8d9
qinqichen <qinqichen@163.com>
2023-11-01 22:09:13 +0800

    菜品新增业务代码

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,4 +1,12 @@
 package com.sky.service;
 
+import com.sky.dto.DishDTO;
+
 public interface DishService {
+
+    /**
+     * 添加菜品
+     * @param dishDTO
+     */
+    void saveWithFlavor(DishDTO dishDTO);
 }
